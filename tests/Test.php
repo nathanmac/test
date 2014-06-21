955acd779665c6011b04419c52baa46e62ca2f09
Nathan Macnamara <nathan.macnamara@arrowecs.co.uk>
2014-06-21 20:06:28 +0100

    Add tests

---
+++ tests/Test.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: nathan
+ * Date: 21/06/2014
+ * Time: 20:04
+ */
+
+namespace tests;
+
+
+class Test extends \PHPUnit_Framework_TestCase {
+
+    public function testConnection()
+    {
+        $conn_id = ftp_connect('127.0.0.1') or \PHPUnit_Framework_Assert::fail('failed to connect');
+    }
+}
+ 
